9dd999f4797f5c99c060182e0efe15917803bdc8
Christian Freitas <chrfreitas@gmail.com>
2021-05-07 11:43:24 -0300

    Create method to delete tasks

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -14,4 +14,20 @@ class TaskController < ApplicationController
       render :json => { error: 'Params are incorrect!' }, status: :bad_request
     end    
   end
+
+  def show
+    task = Task.find_by id: params[:id]
+    render :json => task
+  end
+
+  def destroy
+    task = Task.find_by id: params[:id]
+
+    if task.nil?
+      render :json => { error: 'Task doesn\'t exist'}, status: :not_found
+    else 
+      task.destroy
+      render :json => {id: params[:id]}
+    end
+  end
 end
