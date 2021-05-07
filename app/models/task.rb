c8bd54c514731de41c4f3b807640ed8787f33f32
Christian Freitas <chrfreitas@gmail.com>
2021-05-07 14:41:17 -0300

    Add record callback to task when save and delete

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,14 @@
 class Task < ApplicationRecord
+  after_save :save_log
+  after_destroy :destroy_log
+
   validates :name, :description, presence: true
+ 
+  def save_log
+    puts "Taks saved!"
+  end
+
+  def destroy_log
+    puts "Taks deleted!"
+  end
 end
